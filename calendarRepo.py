e699c5e8d6c0badfcc77f36e2767c0f7ab8cba22
David Beales <David.Beales@ukho.gov.uk>
2020-04-02 08:29:59 +0100

    Swap backend to sql instance

--- calendarRepo.py
+++ calendarRepo.py
@@ -1,3 +1,6 @@
+import secrets
+import pyodbc
+
 import sqlite3
 import uuid
 
@@ -6,43 +9,51 @@ from flask import g
 import config
 configInstance = config.Config()
 
+secrets = secrets.Config()
+
+
 class CalendarRepo(object):
     def __name__(self):
         return 'calendarRepo'
 
     def __init__(self):
+        self.odbc_connection = pyodbc.connect(
+            driver='{SQL Server}',
+            host='tcp:' + secrets.SERVER,
+            database=secrets.DATABASE,
+            trusted_connection='no',
+            user=secrets.USERNAME,
+            password=secrets.PASSWORD)
+        
         self.table_name = configInstance.TABLE_NAME
-        self.database_name =  configInstance.DATABASE_NAME
 
-    def _init(self):
-        self.connection = getattr(g, '_database', None)
-        if self.connection is None:
-            self.connection = g._database = sqlite3.connect(self.database_name)
-
-        self.cursor = self.connection.cursor()
+        self.cursor = self.odbc_connection.cursor()
 
         if self.checkTableExists() is False:
             self.createTable()
+        
+        self.database_name = configInstance.DATABASE_NAME
+
+    def _init(self):
+        pass
 
     def _del(self, self_from_caller):
-        self.connection = getattr(g, '_database', None)
-        if self.connection is not None:
-            self.connection.close()
+        pass
 
     def createTable(self):
-        create_table_sql = """CREATE TABLE IF NOT EXISTS {0}
-                          (
-                           guid UNIQUEIDENTIFIER,
-                           title STRING,
-                           year SHORT,
-                           month SHORT,
-                           day SHORT,
-                           hour SHORT,
-                           minute SHORT
-                           )""".format(self.table_name)
+        create_table_sql = '''if not exists (select * from sysobjects where name='{0}' and xtype='U')
+                            CREATE TABLE {0} (
+                                            guid UNIQUEIDENTIFIER,
+                                            title VARCHAR,
+                                            year INT,
+                                            month INT,
+                                            day INT,
+                                            hour INT,
+                                            minute INT
+                                            )'''.format(self.table_name)
 
         self.cursor.execute(create_table_sql)
-        self.connection.commit()
+        self.odbc_connection.commit()
 
     def insertRow(self, title, year, month, day, hour, minute):
         guid = uuid.uuid4()
@@ -50,7 +61,7 @@ class CalendarRepo(object):
                                (guid, title, year, month, day, hour, minute)
                                VALUES('{0}', '{1}', {2}, {3}, {4}, {5}, {6})
                             """.format(guid, title, year, month, day, hour, minute))
-        self.connection.commit()
+        self.odbc_connection.commit()
         return guid
 
     def updateRow(self, guid, title, year, month, day, hour, minute):
@@ -63,34 +74,39 @@ class CalendarRepo(object):
                                minute = '{5}'
                                WHERE guid = '{6}';
                             """.format(title, year, month, day, hour, minute, guid))
-        self.connection.commit()
+        self.odbc_connection.commit()
 
     def deleteRow(self, guid):
         self.cursor.execute("""DELETE FROM calendar
                                WHERE guid = '{}';
                             """.format(guid))
-        self.connection.commit()
+        self.odbc_connection.commit()
 
     def getRecordsByYearAndMonth(self, year, month):
         self.cursor.execute(
             'SELECT * FROM calendar WHERE year={} AND month={}'.format(year, month))
-        return self.cursor.fetchall()
+        return self.fetchAll()
 
     def getRecordById(self, guid):
         self.cursor.execute(
             'SELECT * FROM calendar WHERE guid="{}"'.format(guid))
-        return self.cursor.fetchone()
+        return self.fetchAll()[0]
 
     def getAll(self):
         self.cursor.execute('SELECT * FROM calendar')
-        return self.cursor.fetchall()
+        return self.fetchAll()
 
     def checkTableExists(self):
-        self.cursor.execute(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name='{0}';".format(self.table_name))
-        data = self.cursor.fetchall()
+        self.cursor.execute("select * from sysobjects where name='{0}' and xtype='U'".format(self.table_name))
+        data = self.fetchAll()
         if len(data) == 0:
             print('There is no table named: {}'.format('calendar'))
             return False
         else:
             return True
+    
+    def fetchAll(self):
+        rows = []
+        for row in self.cursor:
+            rows.append(row)
+        return rows
